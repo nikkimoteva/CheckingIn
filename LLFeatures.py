839acca87532972a6f6aa4d360e7828c9751be0f
Nikki <nikkimoteva@gmail.com>
2019-05-15 11:33:35 -0700

    added the feature of opening up the dictionary on demand

--- LLFeatures.py
+++ LLFeatures.py
@@ -1,4 +1,5 @@
 import TaskLL
+import NotificationAnnoyer
 import random as r
 
 linked_list = TaskLL.DoublyLinkedList()
@@ -31,6 +32,29 @@ def p_k(k):
         temp = temp.next
         t -= 1
 
+
+def redata_node(old_data, new_data):
+    temp = linked_list.head
+    while temp is not None:
+        if temp.data == old_data:
+            temp.data = new_data
+            return
+        else:
+            temp = temp.next
+    print "ERROR you shouldn't have reached here NotoficationAnnoyer must have stopped you before"
+
+
+def open_task(data):
+    a = raw_input("would you like to see the content of this task?(yes/no) ")
+    if a == "yes":
+        print NotificationAnnoyer.tasks_dict[data]
+    elif a == "no":
+        print "Okay, moving on then"
+    else:
+        print "please enter either yes or no"
+        open_task(data)
+
+
 # test to see if linked list works
 def main():
     d = TaskLL.Node(data="task5")
@@ -40,14 +64,14 @@ def main():
     linked_list.insert("task3")
     linked_list.insert("task4")
     linked_list.insert("task9")
+    add_node(d)
     print len(linked_list)
     print linked_list.print_k(2)
     print linked_list.print_list()
     print p_k(2)
     print random()
-
-    add_node(d)
-    print linked_list.print_list()
+    print redata_node("task0", "task9999")
+    print open_task("task1")
 
 
 if __name__ == "__main__":
