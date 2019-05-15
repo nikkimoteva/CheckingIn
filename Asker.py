d6035ca0d5a7be027e4f481aed59b184f683c361
Nikki <nikkimoteva@gmail.com>
2019-05-15 13:37:07 -0700

    Increased the number of options for new features

--- Asker.py
+++ Asker.py
@@ -1,11 +1,25 @@
 import NotificationAnnoyer
+import LLFeatures
 
 def main():
     # I want to ask if they've done the particular task
-    task = raw_input("please enter the title of a task: ")
-    NotificationAnnoyer.ask(task)
-    NotificationAnnoyer.another_task()
-    print "tasks manipulated: ", task
+    ans = raw_input("please select an option:\n"
+                    "1. entering a task\n"
+                    "2. taking a look at a task\n"
+                    "3. flip through tasks\n")
+    if ans == "1":
+        task = raw_input("please enter the title of a task: ")
+        NotificationAnnoyer.ask(task)
+        NotificationAnnoyer.another_task()
+        print "tasks manipulated: ", task
+    elif ans == "2":
+        NotificationAnnoyer.look_at_task()
+    elif ans == "3":
+        LLFeatures.go_through()
+    else:
+        print "\nplease enter one of the available options"
+        main()
+
 
 if __name__ == '__main__':
     main()
