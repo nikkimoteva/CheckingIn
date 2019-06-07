3532c95e016590919159052ee94ff587bf34e15d
Nikki <nikkimoteva@gmail.com>
2019-06-06 18:27:18 -0700

    Added a new database using python shelve to store the tasks as a dictionary

--- Data.py
+++ Data.py
@@ -1,35 +1,50 @@
 import shelve
+import contextlib
+import NotificationAnnoyer
+import cPickle
 
+# TODO use cPickle to save the keys as non-strings and their actual type
+
+
+# task is string, the key of task_dict
 def initial_data():
     # tasks
-    d = shelve.open('TODO', 'c')
-    thistask = {
-        "take a bath": "task1",
-        "shower": "task2",
-        "study": "task3",
-        "cry": "task4"
-      }
-    d['tasks'] = thistask
-    read_task()
-    write_task(task)
-    read_task()
-    d.close()
+    with contextlib.closing(shelve.open("TODO", 'c')) as shelf:
+        shelf["task-1"] = {
+                "title": "pay rent",
+                "deadline": "2019-05-20",
+                "deadtime": "23:59:59",
+                "start date": "2019-05-04",
+                "start time": "15:45:57",
+                "time left": NotificationAnnoyer.time_left("2019-05-20", "23:59:59"),
+                "Recommendation": NotificationAnnoyer.Recommendations.give_recommend(
+                    NotificationAnnoyer.dead("2019-05-20", "23:59:59"), "task-1", NotificationAnnoyer.the_list)
+        }
+        shelf.close()
 
 
 def write_task(task):
     # need to append here instead of over-writing
-    shelf = shelve.open('TODO', 'c')
-    temp = shelf["tasks"]
-    temp.update(task)
-    shelf["tasks"] = temp
-    shelf.close()
-    read_task()
+    with contextlib.closing(shelve.open('TODO', 'c', writeback=True)) as shelf:
+        shelf[task] = NotificationAnnoyer.tasks_dict[task]
+
 
 def read_task():
-    shelf = shelve.open('TODO', 'r')
-    for key in shelf.keys():
-        #repr(key)
-        print key, shelf[key]
-        print key
-        print shelf[key]
-    shelf.close()
+    with contextlib.closing(shelve.open("TODO", 'r')) as shelf:
+        for key in shelf.keys():
+            print repr(key), repr(shelf[key])
+
+
+def delete_data(task):
+    with contextlib.closing(shelve.open("TODO", 'c')) as shelf:
+        try:
+            del shelf[task]
+        except KeyError:
+            print "key does not exist in database"
+
+
+def print_data():
+    with contextlib.closing(shelve.open("TODO", 'r')) as shelf:
+        for key, value in shelf.iteritems():
+            print repr(key), repr(value)
+    NotificationAnnoyer.another_task()
