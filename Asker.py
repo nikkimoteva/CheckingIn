9365c1d118f1b6af1f8fe9d6906b54716bf30042
Nikki <nikkimoteva@gmail.com>
2019-10-13 18:38:17 -0700

    Added option for using within kivy module

--- Asker.py
+++ Asker.py
@@ -15,30 +15,33 @@ def main(arg):
                         "3. flip through tasks\n"
                         "4. quick tasks\n"
                         "5. exit\n")
-        if ans == "1":
-            task = raw_input("please enter the title of a task: ")
-            NotificationAnnoyer.ask(task)
-            Data.dump(NotificationAnnoyer.tasks_dict)
-            NotificationAnnoyer.another_task()
-            print "tasks manipulated: ", task
-        elif ans == "2":
-            NotificationAnnoyer.look_at_task()
-        elif ans == "3":
-            LLFeatures.go_through()
-            return
-        elif ans == "4":
-            try:
-                QuickTasks.loading_tasks()
-                QuickTasks.entering_info_question()
-            except:
-                print "except here"
-                QuickTasks.quick()
-        elif ans == "5":
-            Data.dump(NotificationAnnoyer.tasks_dict)
-            Data.quick_dump(QuickTasks.quick_t)
-        else:
-            print "\nplease enter one of the available options"
-            main()
+        options(ans)
+
+def options(ans):
+    if ans == "1":
+        task = raw_input("please enter the title of a task: ")
+        NotificationAnnoyer.ask(task)
+        Data.dump(NotificationAnnoyer.tasks_dict)
+        NotificationAnnoyer.another_task()
+        print "tasks manipulated: ", task
+    elif ans == "2":
+        NotificationAnnoyer.look_at_task()
+    elif ans == "3":
+        LLFeatures.go_through()
+        return
+    elif ans == "4":
+        try:
+            QuickTasks.loading_tasks()
+            QuickTasks.entering_info_question()
+        except:
+            print "except here"
+            QuickTasks.quick()
+    elif ans == "5":
+        Data.dump(NotificationAnnoyer.tasks_dict)
+        Data.quick_dump(QuickTasks.quick_t)
+    else:
+        print "\nplease enter one of the available options"
+        main()
 
 
 if __name__ == '__main__':
