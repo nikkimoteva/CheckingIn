24eacc72b5e4db01e1dbbd988e4e933bbff4f51d
Nikki <nikkimoteva@gmail.com>
2019-10-13 17:08:12 -0700

    Added the login page

--- Application/startPage.py
+++ Application/startPage.py
@@ -1,42 +1,112 @@
-import kivy
-from kivy.app import App as app
-from kivy.uix.label import Label as label
-kivy.require('1.0.6')
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from db import DataBase
+#import Data
+#use this import to retrieve the information and return for the user
 
 
-class checkingIn(app):
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+
+                self.reset()
+
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                  content=Label(text='Invalid username or password.'),
+                  size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                  content=Label(text='Please fill in all inputs with valid information.'),
+                  size_hint=(None, None), size=(400, 400))
+
+    pop.open()
+
+
+kv = Builder.load_file("checkingin.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+
+class CheckingIn(App):
     def build(self):
-        return label(text = " welcome to checking in woo \n"
-                            " Ahaaaaaa (????)")
-
-class MyWidget(Widget):
-    def __init__(self, **kwargs):
-        super(MyWidget, self).__init__(**kwargs)
-        with self.canvas:
-            self.rect = Rectangle(pos=self.pos, size=self.size)
-
-        self.bind(pos=self.update_rect)
-        self.bind(size=self.update_rect)
-
-    def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
+        return sm
 
 
-class MyWidget(Widget):
-    def __init__(self, **kwargs):
-        super(MyWidget, self).__init__(**kwargs)
-        self.draw_my_stuff()
-
-        self.bind(pos=self.draw_my_stuff)
-        self.bind(size=self.draw_my_stuff)
-
-    def draw_my_stuff(self):
-        self.canvas.clear()
-
-        with self.canvas:
-            self.rect = Rectangle(pos=self.pos, size=self.size)
-
-
-if __name__ == '__main__':
-    checkingIn().run()
+if __name__ == "__main__":
+    CheckingIn().run()
