58af41776cd0e451ea8a39b76c63dfef4da1cb32
Nikki <nikkimoteva@gmail.com>
2019-12-15 17:07:29 -0800

    Added the front end to adding tasks in the kivy file

--- startPage.py
+++ startPage.py
@@ -1,12 +1,19 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+import kivy.event
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from db import DataBase
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.utils import platform
 import NotificationAnnoyer
 import Asker
+import Data
+
 
 
 class AskerPage(Screen):
@@ -14,7 +21,33 @@ class AskerPage(Screen):
 
     def options(self):
         NotificationAnnoyer.adding_dict()
-        Asker.options(self.option.text)
+        sm.current = "adding_task"
+
+
+class AddingTask(Screen):
+    task_title = ObjectProperty(None)
+    task_key = ObjectProperty(None)
+    deadline = ObjectProperty(None)
+    deadtime = ObjectProperty(None)
+    """title = task_title.text
+    key = "task" + task_key.text
+    line = deadline
+    time = deadtime"""
+
+    def adding(self):
+        NotificationAnnoyer.adding_task(self.task_title.text, "task" + self.task_key.text,
+                                        self.deadline.text, self.deadtime.text)
+        Data.dump(NotificationAnnoyer.tasks_dict)
+        task_added()
+        sm.current = "asker"
+
+    def go_back(self):
+        sm.current = "asker"
+
+
+
+#class NAPage(Screen):
+    #task = NotificationAnnoyer.
 
 
 class CreateAccountWindow(Screen):
@@ -92,6 +125,12 @@ def invalidLogin():
                   size_hint=(None, None), size=(400, 400))
     pop.open()
 
+def task_added():
+    pop = Popup(title='Task added',
+                content=Label(text='The current task was added to the list.'),
+                size_hint=(None, None), size=(400, 400))
+    pop.open()
+
 
 def invalidForm():
     pop = Popup(title='Invalid Form',
@@ -100,6 +139,11 @@ def invalidForm():
 
     pop.open()
 
+def wind():
+    if platform not in ('android', 'ios'):
+        Config.set('graphics', 'resizable', '0')
+        Window.size = (320, 420)
+
 
 kv = Builder.load_file("checkingin.kv")
 
@@ -107,11 +151,11 @@ sm = WindowManager()
 db = DataBase("users.txt")
 
 screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"), MainWindow(name="main"),
-           AskerPage(name="asker")]
+           AskerPage(name="asker"), AddingTask(name="adding_task")]
 for screen in screens:
     sm.add_widget(screen)
 
-sm.current = "login"
+sm.current = "adding_task"
 
 
 class CheckingIn(App):
